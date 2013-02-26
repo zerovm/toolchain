4143476ebcd88d83c538a0f6abf51de5e54836ee
YaroslavLitvinov <yaroslav.litvinov@gmail.com>
2013-02-25 20:21:35 -0600

    build script for glibc

--- build_zlibc.sh
+++ build_zlibc.sh
@@ -4,17 +4,17 @@ MAKE="make -j8"
 TOOLCHAINLOC=~
 
 rm BUILD/stamp-glibc64 -f
-echo ${MAKE} -C${ZRT_ROOT} zlibc_dep
+
+${MAKE} -C${ZRT_ROOT} cleandep
 ${MAKE} -C${ZRT_ROOT} zlibc_dep
-ls ${ZRT_ROOT}/lib/zrt.o -l
 
-#${MAKE} BUILD/stamp-glibc64 TOOLCHAINLOC=${TOOLCHAINLOC}
-#${MAKE} export-headers TOOLCHAINLOC=${TOOLCHAINLOC}
-#${MAKE} glibc-adhoc-files TOOLCHAINLOC=${TOOLCHAINLOC}
+if [ ! -f ${ZRT_ROOT}/lib/zrt.o ]
+then
+	echo "${ZRT_ROOT}/lib/zrt.o file not exist"
+	exit
+fi
 
-echo ${MAKE} install-glibc64 TOOLCHAINLOC=${TOOLCHAINLOC}
 ${MAKE} install-glibc64 TOOLCHAINLOC=${TOOLCHAINLOC}
 
-echo ${MAKE} -C${ZRT_ROOT} cleanzrt0
-${MAKE} -C${ZRT_ROOT} cleanzrt0
+${MAKE} -C${ZRT_ROOT} cleandep
 
