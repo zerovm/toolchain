57f21fc6c935a5a9207f5bb8fce5c1f8896e17b3
YaroslavLitvinov <yaroslav.litvinov@gmail.com>
2013-06-06 13:20:14 +0300

    glibc logging pruned

--- build_zlibc.sh
+++ build_zlibc.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 MAKE="make -j16"
-TOOLCHAINLOC=~
 
 rm BUILD/stamp-glibc64 -f
 
 GLIBC_CONFIG="--with-zrt=yes" \
-GLIBC_CFLAGS="-DLIBC_ENOSYS_DEBUG" \
-${MAKE} install-glibc64 TOOLCHAINLOC=${TOOLCHAINLOC}
+${MAKE} install-glibc64
--- build_zlibc_static.sh
+++ build_zlibc_static.sh
@@ -6,5 +6,4 @@ TOOLCHAINLOC=~
 rm BUILD/stamp-glibc64 -f
 
 GLIBC_CONFIG="--enable-shared=no --with-zrt=yes" \
-GLIBC_CFLAGS="-DLIBC_ENOSYS_DEBUG" \
 ${MAKE} install-glibc64 TOOLCHAINLOC=${TOOLCHAINLOC}
